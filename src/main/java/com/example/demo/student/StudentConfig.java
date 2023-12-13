d560124eb84c3f243f399bd40e1c4faf2a0f725e
beyza.gumus <beyza.gumus>
2023-12-13 16:30:11 +0300

    Save to DB -> save students to DB

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+//add student to DB
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.JANUARY;
+
+@Configuration
+public class StudentConfig {
+
+    //with this method we will access to our repository
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(2000, JANUARY,5),
+                    21
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, JANUARY,5),
+                    21
+            );
+
+            studentRepository.saveAll(List.of(mariam,alex));
+        };
+    }
+}
