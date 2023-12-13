b1f2da55893f821bbff3c76831bbfe26fccc404d
beyza.gumus <beyza.gumus>
2023-12-13 17:31:40 +0300

    PUT Request -> update information in DB ; request for changing mariam name to maria : PUT http://localhost:8080/api/v1/student/1?name=Maria and PUT http://localhost:8080/api/v1/student/1?name=Maria&email=mari@gmail.com

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -38,5 +40,29 @@ public class StudentService {
             throw new IllegalStateException("student with id " + studentId + " does not exists");
         }
         studentRepository.deleteById(studentId);
+    }
+
+
+    //thanks to Transactional we are not using any queries like in StudentRepositories
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exists"));
+
+        if(name !=null && name.length()>0 &&
+        !Objects.equals(student.getName(),name)) {
+            student.setName(name);
+        }
+
+        if(email !=null && email.length()>0 &&
+                !Objects.equals(student.getEmail(),email)) {
+
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
