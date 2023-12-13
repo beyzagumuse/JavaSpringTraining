23d45f1fa7642957f5ace2c6d313829ee6847d72
beyza.gumus <beyza.gumus>
2023-12-13 16:57:32 +0300

    Writing Business Logic -> Optional method used, student controlled when added completed

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 //for anything to access my db, needed spesifically when working with JPA
 //this interface is responsible for data access, everything for Data Access Layer
@@ -9,4 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    //SELECT * FROM student WHERE email = ?, the Optional under this equal to this SQL
+    //@Query("SELECT s FROM Student s WHERE s.email=?1") this is jbql(?) version
+    Optional<Student> findStudentByEmail(String email);
 }
