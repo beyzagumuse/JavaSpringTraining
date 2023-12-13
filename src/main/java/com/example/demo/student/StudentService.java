a34440c2b230249eaed34ff73e9bf283d019a45d
beyza.gumus <beyza.gumus>
2023-12-13 15:49:41 +0300

    Properties File, Connection to DB -> Database connected

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -7,12 +7,8 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-@Service // to use this class with @Autowired I have to add @Component or more specific @Service so this can be found
+@Service
 public class StudentService {
-
-    //Business Layer -> API Layer should talk to Service Layer to get some data
-    //and service layer should talk to data access layer to take data and all the way back.
-    // Service Layer mainly responsible for Business Logic.
 
     public List<Student> getStudents(){
         return List.of(
@@ -23,7 +19,5 @@ public class StudentService {
                         LocalDate.of(2000, Month.JANUARY,5),
                         21
                 ));
-        //we take this method from Controller and changed there but this is still a static list
-        //we want this informations to come from database but İt has time
     }
 }
